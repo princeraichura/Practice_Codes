cf2ad4bc54c141d913e4c4eddd877c02c208f6c0
Parth Raichura <parth.raichura@softnautics.com>
2022-09-06 23:39:50 +0530

    example_2

---
+++ MUFFINS3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,n;
+    cin>>t;
+    for (int i=0;i<t;i++)
+    {
+        cin>>n;
+        int x=n/2 +1;
+        cout<<x<<endl;
+    }
+    
+	// your code goes here
+	return 0;
+}
