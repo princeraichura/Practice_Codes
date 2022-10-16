19e1660980f428518be9d3f8b8513e67f46b8548
princeraichura <raichura72@gmail.com>
2022-10-16 23:05:08 +0530

    example_29

---
+++ EQCARDGAME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, n;
+	cin >> t;
+	while (t--) {
+	    cin >> n;
+	    cout << 52 % n << endl;
+	}
+	return 0;
+}
+
