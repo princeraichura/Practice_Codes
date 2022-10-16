e7e7798421aac6b0a4d3de0d92f36121fcda6f25
princeraichura <raichura72@gmail.com>
2022-10-16 23:12:46 +0530

    example_31

---
+++ ODDPAIRS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int t;
+   cin>>t;
+   while(t--){
+       unsigned long int n,a,b;
+       
+       cin>>n;
+       a=n/2;
+       b=n-a;
+       
+       cout<<2*(a*b)<<endl;;
+       
+       
+   }
+	return 0;
+}
