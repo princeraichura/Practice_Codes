766db3b6b0a3686e7a958e5c2dea0da3e2bc5bf9
princeraichura <raichura72@gmail.com>
2022-10-20 21:57:28 +0530

    example_32

---
+++ ADJSUMPAR.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; cin>>t;
+	while(t--){
+	    int n; cin>>n;
+	    int a[n];
+	    int sum = 0;
+	    for(int i=0; i<n; i++)
+	        cin>>a[i];
+	    for(int i=0; i<n; i++)
+	    {
+	        sum = sum + a[i];
+	    }
+	    if(sum%2==0){
+	        cout<<"Yes"<<endl;
+	    }
+	    else
+	        cout<<"No"<<endl;
+	}
+	return 0;
+}
+
